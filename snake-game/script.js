22d518b3270baf4d87e1806ab69a046e7081bcb1
msa-developer <shoaib9542186823@gmail.com>
2025-12-17 20:14:14 +0530

    fixed the board blocks correctly

--- snake-game/script.js
+++ snake-game/script.js
@@ -1,5 +1,11 @@
 const board = document.querySelector(".board");
-const blockHeight = 30;
-const blockWidth = 30;
-
+const blockHeight = 20;
+const blockWidth = 20;
+const rows = Math.floor(board.clientHeight / blockHeight);
 const cols = Math.floor(board.clientWidth / blockWidth);
+
+for (let i = 0; i < rows * cols; i++) {
+  const block = document.createElement("div");
+  block.classList.add("block");
+  board.appendChild(block);
+}
