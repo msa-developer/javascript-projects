519e9963af681a524cd8d9519421da3ee14680df
msa-developer <shoaib9542186823@gmail.com>
2025-12-18 09:45:47 +0530

    perfectly fixed the issues with the board and block classes

--- snake-game/script.js
+++ snake-game/script.js
@@ -1,10 +1,12 @@
 const board = document.querySelector(".board");
-const blockHeight = 30;
-const blockWidth = 30;
-const rows = Math.floor(board.clientHeight / blockHeight);
-const cols = Math.floor(board.clientWidth / blockWidth);
 
-for (let i = 0; i < rows * cols; i++) {
+const blockHeight = 20;
+const blockWidth = 20;
+
+const cols = Math.floor(board.clientWidth / blockWidth);
+const rows = Math.floor(board.clientHeight / blockHeight);
+
+for (let i = 0; i < cols * rows; i++) {
   const block = document.createElement("div");
   block.classList.add("block");
   board.appendChild(block);
