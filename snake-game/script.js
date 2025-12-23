960d498c500e4ea2fcdcb1822485d7b5717965b1
msa-developer <shoaib9542186823@gmail.com>
2025-12-23 18:02:34 +0530

    started logic building for snake

--- snake-game/script.js
+++ snake-game/script.js
@@ -5,12 +5,28 @@ const blockWidth = 80;
 
 const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
+const blocks = [];
+const snake = [
+  {
+    x: 1,
+    y: 3,
+  },
+  {
+    x: 1,
+    y: 4,
+  },
+  {
+    x: 1,
+    y: 5,
+  },
+];
 
 for (let row = 0; row < rows; row++) {
   for (let col = 0; col < cols; col++) {
     const block = document.createElement("div");
     block.classList.add("block");
     board.appendChild(block);
-    block.innerText = `( ${row},${col} )`;
+    block.innerText = `${row}-${col}`;
+    blocks[`${row}-${col}`] = block;
   }
 }
