54b4c39a63bb9b8a5f7f9d6fbe9950379476cab5
msa-developer <shoaib9542186823@gmail.com>
2025-12-23 11:59:35 +0530

    added temporary positions in the blocks to identify positions

--- snake-game/script.js
+++ snake-game/script.js
@@ -1,13 +1,16 @@
 const board = document.querySelector(".board");
 
-const blockHeight = 20;
-const blockWidth = 20;
+const blockHeight = 80;
+const blockWidth = 80;
 
 const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
 
-for (let i = 0; i < cols * rows; i++) {
-  const block = document.createElement("div");
-  block.classList.add("block");
-  board.appendChild(block);
+for (let row = 0; row < rows; row++) {
+  for (let col = 0; col < cols; col++) {
+    const block = document.createElement("div");
+    block.classList.add("block");
+    board.appendChild(block);
+    block.innerText = `( ${row},${col} )`;
+  }
 }
