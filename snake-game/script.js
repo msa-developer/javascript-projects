b6e886f9a228276732551f2e2af385193251e306
msa-developer <shoaib9542186823@gmail.com>
2025-12-24 11:52:09 +0530

    snake funciton initilization for rendering snake

--- snake-game/script.js
+++ snake-game/script.js
@@ -1,14 +1,32 @@
 const board = document.querySelector(".board");
 
-const rowHeight = 22;
-const columnWidth = 22;
+const rowHeight = 80;
+const columnWidth = 80;
 
 const rows = Math.floor(board.clientHeight / rowHeight);
 const columns = Math.floor(board.clientWidth / columnWidth);
+const blocks = [];
+const snakeArr = [
+  { x: 1, y: 0 },
+  { x: 1, y: 1 },
+  { x: 1, y: 2 },
+];
 
 for (let row = 0; row < rows; row++) {
   for (let column = 0; column < columns; column++) {
     const block = document.createElement("div");
+    block.classList.add("block");
     board.appendChild(block);
+    block.innerText = `${row}-${column}`;
+    blocks[`${row}-${column}`] = block;
   }
 }
+
+const renderSnake = () => {
+  snakeArr.forEach((block) => {
+    block.appendChild("snake");
+    console.log(block);
+  });
+};
+
+setInterval(() => {}, 300);
