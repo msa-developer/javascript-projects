7d027a71548a0da8641f7fb7103f2f3e1c004a96
msa-developer <shoaib9542186823@gmail.com>
2025-12-28 14:35:26 +0530

    logic for moving the snake up and down left right

--- snake-game/script.js
+++ snake-game/script.js
@@ -5,13 +5,12 @@ const blockWidth = 80;
 
 const rows = Math.floor(board.clientHeight / blockHeight);
 const columns = Math.floor(board.clientWidth / blockWidth);
+
 const blocks = [];
-const snake = [
-  { x: 0, y: 1 },
-  { x: 0, y: 2 },
-  { x: 0, y: 3 },
-  { x: 1, y: 3 },
-];
+
+const snake = [{ x: 0, y: 14 }];
+
+let direction = "left";
 
 for (let row = 0; row < rows; row++) {
   for (let column = 0; column < columns; column++) {
@@ -24,5 +23,34 @@ for (let row = 0; row < rows; row++) {
 }
 
 const renderSnake = () => {
-  snake.forEach((item) => blocks[`${item.x}-${item.y}`].classList.add("snake"));
+  snake.forEach((item) => {
+    blocks[`${item.x}-${item.y}`].classList.add("snake");
+  });
 };
+
+setInterval(() => {
+  let head = null;
+
+  if (direction === "left") {
+    snake.forEach((item) => {
+      head = { x: item.x, y: item.y - 1 };
+    });
+  } else if (direction === "right") {
+    snake.forEach((item) => {
+      head = { x: item.x, y: item.y + 1 };
+    });
+  } else if (direction === "up") {
+    head = { x: item.x - 1, y: item.y };
+  } else if (direction === "down") {
+    head = { x: item.x + 1, y: item.y };
+  }
+
+  snake.forEach((item) => {
+    blocks[`${item.x}-${item.y}`].classList.remove("snake");
+  });
+
+  snake.unshift(head);
+  snake.pop();
+
+  renderSnake();
+}, 300);
