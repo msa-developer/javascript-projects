0f958083d779afe70c25fce53b92bfe8b65974b5
msa-developer <shoaib9542186823@gmail.com>
2025-12-28 12:15:11 +0530

    added logic for directions

--- snake-game/script.js
+++ snake-game/script.js
@@ -42,8 +42,20 @@ setInterval(() => {
   let head = null;
   if (direction === "left") {
     head = { x: snake[0].x, y: snake[0].y - 1 };
+  } else if (direction === "right") {
+    head = { x: snake[0].x, y: snake[0].y + 1 };
+  } else if (direction === "down") {
+    head = { x: snake[0].x + 1, y: snake[0].y };
+  } else if (direction === "up") {
+    head = { x: snake[0].x - 1, y: snake[0].y };
   }
+
+  snake.forEach((item) => {
+    blocks[`${item.x}-${item.y}`].classList.remove("fill");
+  });
+
   snake.unshift(head);
   snake.pop();
+
   renderSnake();
 }, 300);
