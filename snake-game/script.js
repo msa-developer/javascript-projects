df4230f4435801c36720574e3063126de3b9e1fc
msa-developer <shoaib9542186823@gmail.com>
2025-12-29 15:20:22 +0530

    added vim keybindings

--- snake-game/script.js
+++ snake-game/script.js
@@ -5,15 +5,12 @@ const blockWidth = 80;
 
 const rows = Math.floor(board.clientHeight / blockHeight);
 const cols = Math.floor(board.clientWidth / blockWidth);
-let intervalId = null;
 const blocks = [];
-const snake = [{ x: 4, y: 14 }];
+const snake = [{ x: 5, y: 12 }];
+let intervalId = null;
 let direction = "left";
-let food = [
-  {
-    x: Math.floor(Math.random() * rows),
-    y: Math.floor(Math.random() * cols),
-  },
+const food = [
+  { x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols) },
 ];
 
 for (let row = 0; row < rows; row++) {
@@ -21,7 +18,7 @@ for (let row = 0; row < rows; row++) {
     const block = document.createElement("div");
     block.classList.add("block");
     board.appendChild(block);
-    block.textContent = `${row}-${col}`;
+    block.innerText = `${row}-${col}`;
     blocks[`${row}-${col}`] = block;
   }
 }
@@ -29,49 +26,35 @@ for (let row = 0; row < rows; row++) {
 const renderSnake = () => {
   let head = null;
 
-  if (direction === "left") {
-    snake.forEach((item) => {
-      head = { x: item.x, y: item.y - 1 };
-    });
-  } else if (direction === "right") {
-    snake.forEach((item) => {
-      head = { x: item.x, y: item.y + 1 };
-    });
-  } else if (direction === "up") {
-    snake.forEach((item) => {
-      head = { x: item.x - 1, y: item.y };
-    });
-  } else if (direction === "down") {
-    snake.forEach((item) => {
-      head = { x: item.x + 1, y: item.y };
-    });
-  }
-
-  if (head.x < 0 || head.y < 0 || head.x >= rows || head.y >= cols) {
-    alert("Game Over");
-  }
-
   food.forEach((item) => {
     blocks[`${item.x}-${item.y}`].classList.add("food");
   });
+
+  if (direction === "left") {
+    head = { x: snake[0].x, y: snake[0].y - 1 };
+  } else if (direction === "right") {
+    head = { x: snake[0].x, y: snake[0].y + 1 };
+  } else if (direction === "up") {
+    head = { x: snake[0].x - 1, y: snake[0].y };
+  } else if (direction === "down") {
+    head = { x: snake[0].x + 1, y: snake[0].y };
+  }
 
   if (head.x === food.x || head.y === food.y) {
     food.forEach((item) => {
       blocks[`${item.x}-${item.y}`].classList.remove("food");
     });
-    food = [
-      {
-        x: Math.floor(Math.random() * rows),
-        y: Math.floor(Math.random() * cols),
-      },
-    ];
   }
 
+  if (head.x < 0 || head.y < 0 || head.x >= cols || head.y >= rows) {
+    alert("Game Over");
+    clearInterval(intervalId);
+  }
+
+  snake.unshift(head);
   snake.forEach((item) => {
     blocks[`${item.x}-${item.y}`].classList.remove("snake");
   });
-
-  snake.unshift(head);
   snake.pop();
 
   snake.forEach((item) => {
@@ -84,13 +67,13 @@ intervalId = setInterval(() => {
 }, 300);
 
 addEventListener("keydown", (e) => {
-  if (e.key === "ArrowUp") {
-    direction = "up";
-  } else if (e.key === "ArrowDown") {
-    direction = "down";
-  } else if (e.key === "ArrowRight") {
-    direction = "right";
-  } else if (e.key === "ArrowLeft") {
+  if (e.key === "ArrowLeft" || e.key === "h") {
     direction = "left";
+  } else if (e.key === "ArrowRight" || e.key === "l") {
+    direction = "right";
+  } else if (e.key === "ArrowUp" || e.key === "k") {
+    direction = "up";
+  } else if (e.key === "ArrowDown" || e.key === "j") {
+    direction = "down";
   }
 });
